8009570d490a5407dc751da27e9da4d3f2c4639e
Nastya <nastya.rimashevskaya@gmail.com>
2024-05-03 01:58:44 +0300

    thrid

--- js/app.js
+++ js/app.js
@@ -1,29 +1,4 @@
 
-$(document).ready(function() {
-   $('.slider').slick({
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      responsive: [
-         {
-           breakpoint: 800,
-           settings: {
-             centerMode: true,
-             centerPadding: '40px',
-             slidesToShow: 2
-           }},
-           {
-            breakpoint: 700,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 1,
-              dots: true
-            }}]
-   })
-})
-
 
 $(document).ready(function() {
    $('.header_burger').click(function(event) {
